30e4ee3f54859c73ebdadb60dbaacf4bfd57e6be
lilcharlies <72659963+lilcharlies@users.noreply.github.com>
2020-10-10 22:41:25 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 import tweepy
+from tweepy.auth import OAuthHandler
+
 
 auth = tweepy.OAuthHandler("AM48CG88cNoGUVAeX62NXv9Fx", "zsw3lDnFLcJsVmfTkaylnUB5dcqJKCgzheonaIWVO3Nkrgp1FJ")
 auth.set_access_token("1832298932-LABp8frmHUTd4Cztxyrs0wprk4ZpteTY0A3IMLa", "KyOjlSahbtM2LHRWuBSRLnZ5CDLaUo7ylZOZ6CeK5jvlF")
